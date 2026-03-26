513f43377c003d3a5905f428fcb84ea653ff5a72
TexTrue <65154269+TexBlock@users.noreply.github.com>
2026-03-26 20:23:47 +0800

    Initial commit

---
+++ common/src/main/java/com/kaleblangley/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.kaleblangley;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "locks_reworked";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
